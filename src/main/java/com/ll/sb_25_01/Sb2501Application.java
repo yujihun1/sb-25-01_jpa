39fee44cee62b54574d798f05ff42294d4e23311
yujihun <sk0253642@gmail.com>
2025-01-06 11:23:06 +0900

    jpa setting

---
+++ src/main/java/com/ll/sb_25_01/Sb2501Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb_25_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2501Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sb_25_01/Sb2501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb_25_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
