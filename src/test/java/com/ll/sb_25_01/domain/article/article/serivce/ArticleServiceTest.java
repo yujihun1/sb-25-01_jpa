6850db78238cbfdc7c1fbd14646173941e003f75
yujihun <sk0253642@gmail.com>
2025-01-08 11:25:53 +0900

    add articleComment

--- src/test/java/com/ll/sb_25_01/domain/article/article/serivce/ArticleServiceTest.java
+++ src/test/java/com/ll/sb_25_01/domain/article/article/serivce/ArticleServiceTest.java
@@ -29,19 +29,19 @@ public class ArticleServiceTest {
 
     @Autowired
     private MemberService memberService;
+
     @DisplayName("글 작성")
     @Test
-
     void t1() {
         RsData<Article> writeRs = articleService.write(1, "제목", "내용");
         Article article = writeRs.getData();
         assertThat(article.getId()).isGreaterThan(0L);
     }
+
     @DisplayName("1번 글 제목 가져오기")
     @Test
-    void t2(){
+    void t2() {
         Article article = articleService.findById(1L).get();
-
 
 
         assertThat(article.getTitle()).isEqualTo("제목1");
@@ -50,7 +50,7 @@ public class ArticleServiceTest {
 
     @DisplayName("1번 글 작성자는 user1")
     @Test
-    void t3(){
+    void t3() {
         Article article = articleService.findById(1L).get();
 
         Member author = article.getAuthor();
@@ -61,35 +61,46 @@ public class ArticleServiceTest {
     @DisplayName("1번 글 제목 수정")
     @Test
     @Rollback(false)
-    void t4(){
+    void t4() {
         Article article = articleService.findById(1L).get();
-        LocalDateTime oldModifyDate= article.getModifyDate();
+        LocalDateTime oldModifyDate = article.getModifyDate();
 
         Ut.thread.sleep(1000);
 
-        articleService.modify(article,"수정된 제목","수정된 내용");
+        articleService.modify(article, "수정된 제목", "수정된 내용");
 
         Article article_ = articleService.findById(1L).get();
         assertThat(article_.getTitle()).isEqualTo("수정된 제목");
 
     }
 
-    @DisplayName("1번 글의 댓글들을 수정")
+    @DisplayName("1번 글의 댓글들을 추가")
     @Test
     @Rollback(false)
     void t5() {
+        Article article1 = articleService.findById(1L).get();
+        Member member2 = memberService.findById(2L).get();
+        article1.addComment(member2, "댓글 입니다.");
+    }
+
+
+    @DisplayName("1번 글의 댓글들을 수정")
+    @Test
+    void t6() {
         Article article = articleService.findById(1L).get();
-        article.getComments().forEach(comment ->  {
+
+        article.getComments().forEach(comment -> {
             articleService.modifyComment(comment, comment.getBody() + "!!");
         });
     }
 
     @DisplayName("1번 글의 댓글 중 마지막 것을 삭제")
     @Test
-    @Rollback(false)
-    void t6() {
+    void t7() {
         Article article = articleService.findById(1L).get();
+
         ArticleComment lastComment = article.getComments().getLast();
+
         article.removeComment(lastComment);
     }
 }
