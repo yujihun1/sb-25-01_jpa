1964c35be33730e02817803e6b69108971168640
yujihun <sk0253642@gmail.com>
2025-01-15 09:58:46 +0900

    list table

--- src/main/java/com/ll/sb_25_01/global/initData/NotProd.java
+++ src/main/java/com/ll/sb_25_01/global/initData/NotProd.java
@@ -16,6 +16,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.stream.IntStream;
+
 @Profile("!prod")
 @Configuration
 @RequiredArgsConstructor
@@ -40,6 +42,9 @@ public class NotProd {
         if(memberService.count()>0) return ;
         Member member1 = memberService.join("user1", "1234").getData();
         Member member2 = memberService.join("user2", "1234").getData();
+        Member member3 = memberService.join("user3", "1234").getData();
+
+
         Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
         Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
         Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
@@ -51,12 +56,29 @@ public class NotProd {
         article2.addComment(member1, "댓글3");
         article2.addComment(member1, "댓글4");
         article2.addComment(member1, "댓글5");
+        article2.addComment(member1, "댓글6");
+        article1.addComment(member1, "댓글7");
+        article2.addComment(member1, "댓글8");
+        article2.addComment(member1, "댓글9");
+        article2.addComment(member1, "댓글10");
+        article2.addComment(member1, "댓글11");
+        article2.addComment(member1, "댓글12");
+
 
 
         article1.addTag("자바");
         article1.addTag("백엔드");
         article2.addTag("프레임워크", "스프링부트");
 
+        article4.addTag("자바","스프링부트");
+
+        IntStream.rangeClosed(5,120).forEach(
+                i -> {
+                    String title = "제목" +i;
+                    String body = "내용" +i;
+                    articleService.write(member2.getId(), title,body);
+                }
+        );
     }
 
 
