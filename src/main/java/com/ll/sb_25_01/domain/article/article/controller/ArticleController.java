7de5681cf9412f16dfaa368f9d6ca55d6f728c90
yujihun <sk0253642@gmail.com>
2025-01-21 10:30:05 +0900

    pagination 6

--- src/main/java/com/ll/sb_25_01/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb_25_01/domain/article/article/controller/ArticleController.java
@@ -46,6 +46,7 @@ public class ArticleController {
         Page<Article> itemsPage = articleService.search(kwTypes, kw, pageable);
         model.addAttribute("itemsPage",itemsPage);
         model.addAttribute("kwTypesMap", kwTypesMap);
+        model.addAttribute("page", page);
 
         return "article/list";
     }
